2d782c33717da439801f0409720ab8fb664fa82d
Sam Ternent <sam.ternent@gmail.com>
2021-11-24 22:50:37 +0000

    build: scaffold app

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex h-screen w-screen">
+    <div class="md:w-1/4 bg-gray-100">
+      <Sidebar />
+    </div>
+    <div class="flex-1">
+      <Content />
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from './components/Sidebar.vue';
+import Content from './components/Content.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+    Content,
+  },
+};
+
+</script>
